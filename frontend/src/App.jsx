f47687ece76135edbd54b27fc52c8198b0793819
gorkemozbay <gorkemozbay0405@gmail.com>
2025-10-28 18:32:56 +0300

    FE project created

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import HomeScreen from './screens/HomeScreen'
+
+function App() {
+
+
+  return (
+    <>
+      <HomeScreen></HomeScreen>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/screens/HomeScreen.jsx
@@ -0,0 +1,12 @@
+
+function HomeScreen() {
+
+
+    return (
+        <>
+            <h1>HomeScreen</h1>
+        </>
+    )
+}
+
+export default HomeScreen;
