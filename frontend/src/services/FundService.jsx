0a270d2c70f8bfc7ab3d098071398cdbaddc1f7e
gorkemozbay <gorkemozbay0405@gmail.com>
2025-12-22 15:59:42 +0300

    Merge resolved

--- frontend/src/services/FundService.jsx
+++ frontend/src/services/FundService.jsx
@@ -1,12 +1,7 @@
 import axios from "axios";
 
-<<<<<<< HEAD
 //   const BASE_URL = "http://172.20.10.13:8080/api";
 const BASE_URL = "https://fonfon-1045759541438.europe-west6.run.app/api";
-=======
- const BASE_URL = "http://localhost:8080/api";
-//const BASE_URL = "https://fonfon-1045759541438.europe-west6.run.app/api";
->>>>>>> fef7e17604366e41294b6a72f7f38b94a740024b
 
 export const getAllFunds = async () => {
   const res = await axios.get(`${BASE_URL}/funds`);
